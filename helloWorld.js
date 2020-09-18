bf739a856ebc29dc6632b18d415fea7a8b06948e
haneul yeon <yun901130@naver.com>
2020-09-18 14:10:20 +0900

    First commit

---
+++ helloWorld.js
@@ -0,0 +1,13 @@
+console.log('Hello World');
+
+let sum=0;
+for (let i=1; i<=100; i++)
+    sum += i;
+
+console.log(`1에서 100까지 정수의 합은 ${sum}입니다.`)
+console.log('1에서 100까지 정수의 합은 ' + sum + '입니다.');
+
+let msg = `1에서 100까지 정수의 합은
+${sum}
+입니다.`;
+console.log(msg)
