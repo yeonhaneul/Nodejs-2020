c64fd00b9a878593e613605506ee8647082c372e
haneul yeon <yun901130@naver.com>
2020-09-22 14:42:56 +0900

    반복문

--- 04.반복문/07.plusCycle.js
+++ 04.반복문/07.plusCycle.js
@@ -1,4 +1,4 @@
-const readline = require('readline');
+/* const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -21,4 +21,30 @@ rl.on('line', function(number) {
     }
     while (d=number)
 rl.close();
+}); */
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.setPrompt('0~99 사이의 숫자 입력> ');
+rl.prompt();
+
+rl.on('line', function(buf) {
+    let orgNumber = (buf.length > 1) ? buf : '0'+buf[0];
+    let number = orgNumber;
+    let cycle = 0;
+    while(true) {
+        let sum = parseInt(number[0]) + parseInt(number[1]);
+        let newNumber = number [1] + sum % 10;
+        cycle++;
+        console.log(number, newNumber, cycle);
+        if (orgNumber === newNumber)
+            break;
+        if (cycle > 100)
+            break;
+        number = newNumber;
+    }
+    rl.close();
 });
--- 04.반복문/08.perfectNumber.js
+++ 04.반복문/08.perfectNumber.js
@@ -1,8 +1,20 @@
+// 완전수 구하기
 
-for (let i=2; i<10000; i++) {
-    let perfectnum = i;
+for (let i=2; i<=10000; i++) {
+    // 약수 구하기
+    let divArray = [];
     for (let k=1; k<i; k++) {
-        let array = k;
-        console.log(array[k])
+        if (i % k === 0) {
+            divArray.push(k);
         }
+    }
+    //약수의 합 구하기
+    let sum = 0;
+    for (let divisor of divArray)
+        sum += divisor;
+    // 숫자 비교하기
+    if (i === sum) {
+        console.log(i);
+        console.log(divArray);
+    }
 }
