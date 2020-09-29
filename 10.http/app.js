3c98c00f8e87fb972bdc6ed37a81d25a798dc522
haneul yeon <yun901130@naver.com>
2020-09-29 10:55:44 +0900

    app조회

--- 10.http/app.js
+++ 10.http/app.js
@@ -3,23 +3,38 @@ const url = require('url');
 const qs = require('querystring');
 const fs = require('fs');
 const view = require('./view/index');
+const tpl = require('./view/template.js');
+const template = require('./view/template.js');
 
 http.createServer(function(req,res) {
     let pathname = url.parse(req.url).pathname;
+    let query = url.parse(req.url,true).query;
+    console.log(pathname, query.id)
     switch(pathname) {
     case '/':
-        fs.readdir('data', function(error, filelist) {
-            let list = '';
-            for (let file of filelist) {
-                let filename = file.substring(0, file.length-4);
-                list +=  `<li><a href="/?id=${filename}">${filename}</a></li>`;
-            }
-            let html = view.index(list);
-            res.end(html);
-        })
+        if (query.id === undefined) {   // qureyid가 없는경우 초기값이 나오게, id가 들어가면 id값이 화면에 나오게
+            fs.readdir('data', function(error, filelist) {
+                let list = template.listGen(filelist);
+                let content = template.HOME_CONTENTS;
+                let control = template.buttonGen();
+                let html = view.index('Web 기술', list, content, control); // content를 넣을 수 있게된다.
+                res.end(html);
+            });
+        } else {
+            fs.readdir('data', function(error, filelist) {
+                let list = template.listGen(filelist);
+                let title = query.id;
+                let control = template.buttonGen(title);
+                let filename = 'data/' + title + '.txt';
+                fs.readFile(filename, 'utf8', (error, buffer) => {
+                    let html = view.index(title, list, buffer, control);
+                    res.end(html);
+                });
+            });
+        }
         break;
     default:
-        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.writeHead(404);
         res.end();
     }
 }).listen(3000, () => {
---
+++ 10.http/view/template.js
@@ -0,0 +1,33 @@
+module.exports = {
+    HOME_CONTENTS: `
+    웹 개발(web development)은 인터넷(월드 와이드 웹)이나 인트라넷(사설망)을 위한 웹사이트를 개발하는 일을 가리킨다.
+    웹 개발은 가장 단순한 단일 정적 문서의 플레인 텍스트에서부터 가장 복잡한 웹 기반 인터넷 애플리케이션, 전자 비즈니스,
+    소셜 네트워크 서비스에 이르기까지 개발 범위가 다양하다.
+    일반적으로 웹 개발이라 부를 때는 웹 프로그래밍뿐만 아니라 더 포괄적인 작업인 웹 디자인, 웹 콘텐츠 개발,
+    클라이언트 사이드/서버 사이드 스크립트 작업, 웹 서버 및 네트워크 보안 구성, 전자 상업 개발을 아우른다.
+    `,
+    listGen:   function(filelist) {
+        let list = '<ul>\n'
+        for (let file of filelist) {
+            let filename = file.substring(0, file.length-4);
+            list +=  `<li><a href="/?id=${filename}">${filename}</a></li>\n`;
+        }
+        list += '</ul>\n';
+        return list;
+    },
+    buttonGen: function(title) {
+        if (title === undefined) {// 홈화면은 수정/삭제를 할 필요가 없으니 작동하지 않도록
+            return `
+            <button onclick="location.href='/create'">추가</button>
+            <button>수정</button>
+            <button>삭제</button>
+            `;
+        } else {    // id화면에서는 수정/삭제가 필요해서 수정/삭제가 작동하도록
+            return `
+            <button onclick="location.href='/create'">추가</button>
+            <button onclick="location.href='/update?id=${title}'">수정</button>
+            <button onclick="location.href='/delete?id=${title}'">삭제</button>
+        `;
+        }
+    }
+}
